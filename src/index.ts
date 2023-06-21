31fa3cdb99ddfe2862e9cb94056951f39a5fcc8e
Kirill <kirill2494@mail.ru>
2023-06-21 15:52:47 +0400

    some test 2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
